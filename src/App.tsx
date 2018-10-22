6872a4745cb5e187d1bad812412fb1d300b88435
Jeff Friesen <momoblackblack@gmail.com>
2018-10-21 18:13:27 -0600

    Add react router basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
 import * as React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
-import logo from './logo.svg'
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+      <hr />
+
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }: { match: any }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic} />
+    <Route
+      exact={true}
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+)
+
+const Topic = ({ match }: { match: any }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 export default App
