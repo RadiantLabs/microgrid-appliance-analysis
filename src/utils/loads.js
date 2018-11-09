3c61fb3cca589e733da7d5d2f73997d5baa8d936
Jeff Friesen <momoblackblack@gmail.com>
2018-11-09 08:53:00 -0700

    Convert calculateNewLoads to a reduce instead of map function
    * This allows accessing a previously calculated value

--- src/utils/loads.js
+++ src/utils/loads.js
@@ -1,21 +1,6 @@
 import _ from 'lodash'
-import { toJS } from 'mobx'
-import { arrayInsert, setKeyOrder } from './general'
-
-export function calcAvailableCapacity(table, { minBatteryEnergyContent }) {
-  return _.map(table, (row, rowIndex) => {
-    if (rowIndex === 0) {
-      return { ...row, ...{ availableCapacity: 'availableCapacity' } }
-    }
-    if (rowIndex === 1) {
-      return { ...row, ...{ availableCapacity: 'kW' } }
-    }
-    const excessElecProd = row['Excess Electrical Production']
-    const energyContent = row['Generic 1kWh Lead Acid [ASM] Energy Content']
-    const capacity = excessElecProd + energyContent - minBatteryEnergyContent
-    return { ...row, ...{ availableCapacity: capacity } }
-  })
-}
+// import { toJS } from 'mobx'
+// import { arrayInsert, setKeyOrder } from './general'
 
 export function addColumnTitles(columnInfo) {
   return _.mapValues(columnInfo, (val, key) => key)
@@ -39,12 +24,17 @@ export function calculateNewLoads({ table, fields, tableStats, constants }) {
     _prevBatteryEnergyContentDelta: 'kWh',
   }
 
-  const results = _.map(tableData, (row, rowIndex, rows) => {
+  // Reducer function. This is needed so that we can have access to values in
+  // rows we previously calculated
+  const columnReducer = (result, row, rowIndex, rows) => {
+    // Deal with top 2 header rows
     if (rowIndex === 0) {
-      return { ...row, ...addColumnTitles(columnInfo) }
+      result.push({ ...row, ...addColumnTitles(columnInfo) })
+      return result
     }
     if (rowIndex === 1) {
-      return { ...row, ...columnInfo }
+      result.push({ ...row, ...columnInfo })
+      return result
     }
 
     // Grab existing values for this row:
@@ -54,13 +44,11 @@ export function calculateNewLoads({ table, fields, tableStats, constants }) {
 
     // Grab values from previous row
     const prevRow = rowIndex < 3 ? {} : rows[rowIndex - 1]
-    const prevEnergyContent =
-      prevRow['Generic 1kWh Lead Acid [ASM] Energy Content']
+    const prevEnergyContent = prevRow['Generic 1kWh Lead Acid [ASM] Energy Content']
 
     // Calculate new values:
     const newApplianceLoad = row['appliance_load'] // TODO: This will be calculated based on field
-    const availableCapacity =
-      excessElecProd + energyContent - minBatteryEnergyContent
+    const availableCapacity = excessElecProd + energyContent - minBatteryEnergyContent
     const newAvailableCapacity = availableCapacity - newApplianceLoad
     const newUnmetLoad = newAvailableCapacity > 0 ? 0 : -newAvailableCapacity
     const totalUnmetLoad = unmetElecLoad + newUnmetLoad
@@ -68,33 +56,33 @@ export function calculateNewLoads({ table, fields, tableStats, constants }) {
       newApplianceLoad > excessElecProd ? newApplianceLoad - excessElecProd : 0
 
     // Original Battery Energy Content Delta
-    const prevBatteryEnergyContentDelta =
-      rowIndex < 3 ? 0 : energyContent - prevEnergyContent
+    const prevBatteryEnergyContentDelta = rowIndex < 3 ? 0 : energyContent - prevEnergyContent
 
-    // New Battery Energy Content
+    // New Battery Energy Content: This is the hard one and why we need a reducer function instead of map
     // const newBatteryEnergyContent =
     //   prevNewBatteryEnergyContent + // Is this even available yet? This was calculated and returned in the last row
     //   prevBatteryEnergyContentDelta -
     //   newApplianceBatteryConsumption
 
-    return {
+    result.push({
       ...row,
       ...{
         _availableCapacity: _.round(availableCapacity, 2),
         _newAvailableCapacity: _.round(newAvailableCapacity, 2),
         _newUnmetLoad: _.round(newUnmetLoad, 2),
         _totalUnmetLoad: _.round(totalUnmetLoad, 2),
-        _newApplianceBatteryConsumption: _.round(
-          newApplianceBatteryConsumption,
-          2
-        ),
-        _prevBatteryEnergyContentDelta: _.round(
-          prevBatteryEnergyContentDelta,
-          3
-        ),
+        _newApplianceBatteryConsumption: _.round(newApplianceBatteryConsumption, 2),
+        _prevBatteryEnergyContentDelta: _.round(prevBatteryEnergyContentDelta, 3),
       },
-    }
-  })
+    })
+    return result
+  }
 
-  return { tableData: results, keyOrder: keyOrder.concat(_.keys(columnInfo)) }
+  // Iterate over tableData, pushing each new row into an array
+  const withNewColumns = _.reduce(tableData, columnReducer, [])
+
+  return {
+    tableData: withNewColumns,
+    keyOrder: keyOrder.concat(_.keys(columnInfo)),
+  }
 }
