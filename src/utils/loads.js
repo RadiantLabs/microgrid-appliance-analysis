abe7ba86387881257e3dd24e2af4b94911c0086c
Jeff Friesen <momoblackblack@gmail.com>
2018-11-11 10:27:01 -0700

    Documenting calculted columns ***
    * This is important for me to have more confidence in the calculations and my future self (among others)

--- src/utils/loads.js
+++ src/utils/loads.js
@@ -14,6 +14,7 @@ export function addColumnTitles(columnInfo) {
 export function calculateNewLoads({ table, fields, homerStats, constants }) {
   const { tableData, keyOrder } = table
   const { effectiveMinBatteryEnergyContent, minBatteryStateOfCharge } = homerStats
+  const headerRowCount = 2
 
   const columnInfo = {
     availableCapacity: 'kW',
@@ -39,12 +40,13 @@ export function calculateNewLoads({ table, fields, homerStats, constants }) {
     }
 
     // Get the previous row (from the original rows, not the new calculated rows)
-    const prevRow = rowIndex < 3 ? {} : rows[rowIndex - 1]
+    const prevRow = rowIndex <= headerRowCount ? {} : rows[rowIndex - 1]
 
     // Get the previous row from the calculated results (the reason for the reduce function)
-    const prevResult = rowIndex < 3 ? {} : result[rowIndex - 1]
+    const prevResult = rowIndex <= headerRowCount ? {} : result[rowIndex - 1]
 
-    // Get existing values from this row:
+    // Get existing values from the current row we are iterating over:
+    // Excess electrical production: Original energy production minus original load (not new appliances)
     const excessElecProd = row['Excess Electrical Production']
     const batteryEnergyContent = row['Generic 1kWh Lead Acid [ASM] Energy Content']
     const batteryStateOfCharge = row['Generic 1kWh Lead Acid [ASM] State of Charge']
@@ -53,40 +55,75 @@ export function calculateNewLoads({ table, fields, homerStats, constants }) {
     // Get values from previous row
     const prevBatteryEnergyContent = prevRow['Generic 1kWh Lead Acid [ASM] Energy Content']
 
-    // Calculate new values:
+    // Calculate load profile from usage profile
     const newApplianceLoad = row['appliance_load'] // TODO: This will be calculated based on field
 
-    // TODO: Need to implement a calculation that looks at the floor
-    // Dupelicate Adam's spreadsheet and see if I can do Z17>52.46135
+    /*
+     * Now calculate new values based on the HOMER and usage profiles
+     */
+
+    // This is the energy content above what HOMER (or the user) decides is the minimum
+    // Energy content the battery should have
     const energyContentAboveMin = batteryEnergyContent - effectiveMinBatteryEnergyContent
+
+    // Find available capacity (kW) before the new appliance is added
     const availableCapacity =
       excessElecProd + (batteryStateOfCharge <= minBatteryStateOfCharge ? 0 : energyContentAboveMin)
 
+    // Find available capacity after the new appliance is added
     const availableCapacityAfterNewLoad = availableCapacity - newApplianceLoad
+
+    // Is there an unmet load after the new appliance is added?
+    // If there is no available capacity (or goes negative) after the new appliance is added,
+    // then the unmet load equals that (negative) "available" capacity
     const additionalUnmetLoad =
       availableCapacityAfterNewLoad > 0 ? 0 : -availableCapacityAfterNewLoad
+
+    // Add up the original unmet load with no new appliance and now the additional unmet load
+    // now that we have a new appliance on the grid
     const newTotalUnmetLoad = unmetElecLoad + additionalUnmetLoad
+
+    // What is the battery consumption (kW) now that we have a new appliance on the grid?
+    // If the new appliance load is greater than the excess electrical production, we are
+    // draining the battery by the difference between new load and the excess production.
+    // If the excess load is greater than the new appliance load, then we aren't draining
+    // the battery.
+    // excessElecProd is the excess after taking into acount the original load
     const newApplianceBatteryConsumption =
       newApplianceLoad > excessElecProd ? newApplianceLoad - excessElecProd : 0
 
     // Original Battery Energy Content Delta
+    // This is how much the energy content in the battery has increased or decreased in the last hour
+    // Takes into account the 2 column headers that are text, not real values
     const originalBatteryEnergyContentDelta =
-      rowIndex === 2 ? 0 : batteryEnergyContent - prevBatteryEnergyContent
+      rowIndex <= headerRowCount ? 0 : batteryEnergyContent - prevBatteryEnergyContent
 
-    // New Battery Energy Content: This is the hard one and why we need a reducer function instead of map
+    // New Appliance Battery Energy Content:
+    // The battery energy content under the scenario of adding a new appliance.
+    // This requires us to look at the energy content of the battery from the previous hour,
+    // which means we need to look at the previous row than the one we are iterating over.
+    // This is why these values are being calculated in a reducing function instead of a map
     const prevNewApplianceBatteryEnergyContent =
-      rowIndex === 2 ? 0 : prevResult['newApplianceBatteryEnergyContent']
-
+      rowIndex <= headerRowCount ? 0 : prevResult['newApplianceBatteryEnergyContent']
     const newApplianceBatteryEnergyContent =
-      rowIndex === 2
-        ? batteryEnergyContent + newApplianceBatteryConsumption
-        : prevNewApplianceBatteryEnergyContent +
+      rowIndex <= headerRowCount
+        ? // For the first hour (row 3 if there are 2 header rows):
+          // We just look at the energy content of the battery and
+          // how much a new appliance would use from the battery:
+          batteryEnergyContent - newApplianceBatteryConsumption
+        : // For hours after that, we need to take the perspective of the battery if a new
+          // appliance was added. Take the battery energy content we just calculated from the
+          // previous hour:
+          prevNewApplianceBatteryEnergyContent +
+          // Add how much energy content was added or removed from the original load:
           originalBatteryEnergyContentDelta -
+          // Now subtract out any battery consumption the new appliance would use
           newApplianceBatteryConsumption
 
     result.push({
       ...row,
       ...{
+        energyContentAboveMin: _.round(energyContentAboveMin, 3),
         availableCapacity: _.round(availableCapacity, 2),
         availableCapacityAfterNewLoad: _.round(availableCapacityAfterNewLoad, 2),
         additionalUnmetLoad: _.round(additionalUnmetLoad, 2),
