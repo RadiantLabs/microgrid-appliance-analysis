23a7604a3c160c91b5be3044eb22fad9b1bec302
Jeff Friesen <momoblackblack@gmail.com>
2019-03-10 11:22:02 +0700

    Adjust appliace selection table layout

--- src/components/Main/FileChooser/index.js
+++ src/components/Main/FileChooser/index.js
@@ -15,6 +15,93 @@ import {
   Input,
 } from 'semantic-ui-react'
 
+const ApplianceSelectionTrigger = props => {
+  const { loading } = props
+  return (
+    <div {..._.omit(props, ['loading'])} style={{ cursor: 'pointer' }}>
+      TODO show selected appliances here
+      {loading ? (
+        <Loader active inline size="tiny" style={{ paddingLeft: '12px' }} />
+      ) : (
+        <Icon name="dropdown" style={{ paddingLeft: '12px' }} />
+      )}
+    </div>
+  )
+}
+
+const ApplianceSelectionTable = inject('store')(
+  observer(({ store }) => {
+    const {
+      activeAppliance,
+      availableAppliances,
+      activeApplianceIsLoading,
+      setActiveApplianceFile,
+    } = store
+    return (
+      <Table basic="very" compact>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Enable</Table.HeaderCell>
+            <Table.HeaderCell style={{ width: '200px' }}>Label</Table.HeaderCell>
+            <Table.HeaderCell style={{ width: '300px' }}>Description</Table.HeaderCell>
+            <Table.HeaderCell style={{ width: '120px' }}>Cost</Table.HeaderCell>
+            <Table.HeaderCell collapsing>Cost Assignment</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {_.map(availableAppliances, appliance => {
+            const { fileLabel, fileDescription } = appliance
+            return (
+              <Table.Row key={appliance.fileInfo.id}>
+                <Table.Cell collapsing>
+                  <Checkbox toggle checked />
+                </Table.Cell>
+                <Table.Cell>{fileLabel}</Table.Cell>
+                <Table.Cell>{fileDescription}</Table.Cell>
+                <Table.Cell>
+                  <Input
+                    fluid
+                    value={2000}
+                    // onChange={this.handleChange}
+                    // onBlur={this.handleBlur}
+                    error={false}
+                    size="small"
+                    label={{ basic: true, content: '$' }}
+                    labelPosition="left"
+                  />
+                </Table.Cell>
+                <Table.Cell>
+                  <Form style={{ marginTop: '7px', marginBottom: '7px' }}>
+                    <Form.Field style={{ marginBottom: 0 }}>
+                      <Radio
+                        label="Grid Owner"
+                        name="radioGroup"
+                        value="this"
+                        checked={true}
+                        style={{ marginBottom: '3px' }}
+                        // onChange={this.handleChange}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <Radio
+                        label="Appliance Owner"
+                        name="radioGroup"
+                        value="that"
+                        checked={false}
+                        // onChange={this.handleChange}
+                      />
+                    </Form.Field>
+                  </Form>
+                </Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table>
+    )
+  })
+)
+
 class FileChoosers extends Component {
   setActiveGridFile = (event, data) => {
     event.preventDefault()
@@ -81,162 +168,6 @@ class FileChoosers extends Component {
 }
 
 export default inject('store')(observer(FileChoosers))
-
-const ApplianceSelectionTrigger = props => {
-  const { loading } = props
-  return (
-    <div {..._.omit(props, ['loading'])} style={{ cursor: 'pointer' }}>
-      This is the selected appliance 20
-      {loading ? (
-        <Loader active inline size="tiny" style={{ paddingLeft: '12px' }} />
-      ) : (
-        <Icon name="dropdown" style={{ paddingLeft: '12px' }} />
-      )}
-    </div>
-  )
-}
-
-const ApplianceSelectionTable = props => (
-  <Table basic="very" compact>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Enable</Table.HeaderCell>
-        <Table.HeaderCell>Label</Table.HeaderCell>
-        <Table.HeaderCell>Description</Table.HeaderCell>
-        <Table.HeaderCell style={{ width: '120px' }}>Cost</Table.HeaderCell>
-        <Table.HeaderCell>Cost Assignment</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell collapsing>
-          <Checkbox toggle checked />
-        </Table.Cell>
-        <Table.Cell>Rice Mill</Table.Cell>
-        <Table.Cell>A longer rice mill description</Table.Cell>
-        <Table.Cell>
-          <Input
-            fluid
-            value={2000}
-            // onChange={this.handleChange}
-            // onBlur={this.handleBlur}
-            error={false}
-            size="small"
-            label={{ basic: true, content: '$' }}
-            labelPosition="left"
-          />
-        </Table.Cell>
-        <Table.Cell>
-          <Form style={{ marginTop: '7px', marginBottom: '7px' }}>
-            <Form.Field style={{ marginBottom: 0 }}>
-              <Radio
-                label="Grid Owner"
-                name="radioGroup"
-                value="this"
-                checked={true}
-                style={{ marginBottom: '3px' }}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Radio
-                label="Appliance Owner"
-                name="radioGroup"
-                value="that"
-                checked={false}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-          </Form>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell collapsing>
-          <Checkbox toggle />
-        </Table.Cell>
-        <Table.Cell>Maize Mill 1</Table.Cell>
-        <Table.Cell>A longer maize mill 1 description</Table.Cell>
-        <Table.Cell>
-          <Input
-            fluid
-            value={2000}
-            // onChange={this.handleChange}
-            // onBlur={this.handleBlur}
-            error={false}
-            size="small"
-            label={{ basic: true, content: '$' }}
-            labelPosition="left"
-          />
-        </Table.Cell>
-        <Table.Cell>
-          <Form style={{ marginTop: '7px', marginBottom: '7px' }}>
-            <Form.Field style={{ marginBottom: 0 }}>
-              <Radio
-                label="Grid Owner"
-                name="radioGroup"
-                value="this"
-                checked={true}
-                style={{ marginBottom: '3px' }}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Radio
-                label="Appliance Owner"
-                name="radioGroup"
-                value="that"
-                checked={false}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-          </Form>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell collapsing>
-          <Checkbox toggle />
-        </Table.Cell>
-        <Table.Cell>Water Pump</Table.Cell>
-        <Table.Cell>A longer water pump description</Table.Cell>
-        <Table.Cell>
-          <Input
-            fluid
-            value={2000}
-            // onChange={this.handleChange}
-            // onBlur={this.handleBlur}
-            error={false}
-            size="small"
-            label={{ basic: true, content: '$' }}
-            labelPosition="left"
-          />
-        </Table.Cell>
-        <Table.Cell>
-          <Form style={{ marginTop: '7px', marginBottom: '7px' }}>
-            <Form.Field style={{ marginBottom: 0 }}>
-              <Radio
-                label="Grid Owner"
-                name="radioGroup"
-                value="this"
-                checked={true}
-                style={{ marginBottom: '3px' }}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Radio
-                label="Appliance Owner"
-                name="radioGroup"
-                value="that"
-                checked={false}
-                // onChange={this.handleChange}
-              />
-            </Form.Field>
-          </Form>
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-)
 
 /* <Dropdown text={activeAppliance.fileLabel} loading={activeApplianceIsLoading}>
 <Dropdown.Menu>
