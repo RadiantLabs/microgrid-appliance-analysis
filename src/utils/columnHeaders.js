97749e49784f65235f9fcc24618f748052a3ada6
Jeff Friesen <momoblackblack@gmail.com>
2019-03-23 17:55:21 +0700

    Make ordering and specifying data table headers simpler

--- src/utils/columnHeaders.js
+++ src/utils/columnHeaders.js
@@ -1,189 +1,94 @@
 import _ from 'lodash'
 
-// TODO: This could all be done dynamically with computed values, removing
-// the columns I don't want
-export const hybridColumnHeaders = [
-  'hour',
-  'datetime',
-  'hour_of_day',
-  // 'day_hour',
-  'electricalProductionLoadDiff',
-  'totalElectricalLoadServed',
-  // 'newAppliancesLoad',
-  'availableCapacity',
-  'availableCapacityAfterNewLoad',
-  'batteryEnergyContent',
-  'originalUnmetLoad',
-  'additionalUnmetLoad',
-  'newUnmetLoad',
+// Set column order and units for the datatable
+export const hybridColumnHeaderUnits = [
+  ['hour', '-'],
+  ['datetime', '-'],
+  ['hour_of_day', '-'],
+  // ['day_hour', '-'],
+  ['totalElectricalLoadServed', 'kW'],
+  ['electricalProductionLoadDiff', 'kW'],
+  // ['newAppliancesLoad', 'kW'],
+  ['availableCapacity', 'kW'],
+  ['availableCapacityAfterNewLoad', 'kW'],
+  ['batteryEnergyContent', 'kWh'],
+  ['originalUnmetLoad', 'kW'],
+  ['additionalUnmetLoad', 'kW'],
+  ['newUnmetLoad', 'kW'],
 ]
 
-export const hybridColumnHeaderUnits = {
-  hour: '-',
-  datetime: '-',
-  hour_of_day: '-',
-  // day_hour: '-',
-  totalElectricalLoadServed: 'kW',
-  electricalProductionLoadDiff: 'kW',
-  // newAppliancesLoad: 'kW',
-  availableCapacity: 'kW',
-  availableCapacityAfterNewLoad: 'kW',
-  batteryEnergyContent: 'kWh',
-  originalUnmetLoad: 'kW',
-  additionalUnmetLoad: 'kW',
-  newUnmetLoad: 'kW',
-}
-
-export const homerHeaders = [
-  // 'Time',
-  'Global Solar',
-  'PV Solar Altitude',
-  'PV Solar Azimuth',
-  'PV Angle of Incidence',
-  'PV Incident Solar',
-  'PV Power Output',
-  'AC Primary Load',
-  'AC Primary Load Served',
-  'Total Electrical Load Served',
-  'Renewable Penetration',
-  'Original Excess Electrical Production',
-  // 'Unmet Electrical Load',
-  'Capacity Shortage',
-  'Total Renewable Power Output',
-  'totalElectricalProduction',
-  'originalElectricalProductionLoadDiff',
-  'originalBatteryEnergyContent',
-  'originalModeledBatteryEnergyContent',
-  'Inverter Power Input',
-  'Inverter Power Output',
-  'Rectifier Power Input',
-  'Rectifier Power Output',
-  'Original Battery Maximum Charge Power',
-  'Original Battery Maximum Discharge Power',
-  'Original Battery Charge Power',
-  'Original Battery Discharge Power',
-  'Original Battery Input Power',
-  'Original Battery State of Charge',
-  'Original Battery Energy Cost',
-  'Original Battery Temperature',
-  'Original Battery Degradation - Time and Temperature',
-  'Original Battery Degradation - Cycling',
-  'Original Battery Equivalent Cycles',
-  'AC Required Operating Capacity',
-  'DC Required Operating Capacity',
-  'AC Operating Capacity',
-  'DC Operating Capacity',
-  // 'hour',
+export const homerHeaderUnits = [
+  ['Global Solar', 'kW/m2'],
+  ['PV Solar Altitude', 'Â°'],
+  ['PV Solar Azimuth', 'Â°'],
+  ['PV Angle of Incidence', 'Â°'],
+  ['PV Incident Solar', 'kW/m2'],
+  ['PV Power Output', 'kW'],
+  ['AC Primary Load', 'kW'],
+  ['AC Primary Load Served', 'kW'],
+  ['Total Electrical Load Served', 'kW'],
+  ['Renewable Penetration', '%'],
+  ['Original Excess Electrical Production', 'kW'],
+  ['Capacity Shortage', 'kW'],
+  ['Total Renewable Power Output', 'kW'],
+  ['totalElectricalProduction', 'kW'],
+  ['originalElectricalProductionLoadDiff', 'kW'],
+  ['originalBatteryEnergyContent', 'kWh'],
+  ['originalModeledBatteryEnergyContent', 'kWh'],
+  ['Inverter Power Input', 'kW'],
+  ['Inverter Power Output', 'kW'],
+  ['Rectifier Power Input', 'kW'],
+  ['Rectifier Power Output', 'kW'],
+  ['Original Battery Maximum Charge Power', 'kW'],
+  ['Original Battery Maximum Discharge Power', 'kW'],
+  ['Original Battery Charge Power', 'kW'],
+  ['Original Battery Discharge Power', 'kW'],
+  ['Original Battery Input Power', 'kW'],
+  ['Original Battery State of Charge', '%'],
+  ['Original Battery Energy Cost', '$/kWh'],
+  ['Original Battery Temperature', 'C'],
+  ['Original Battery Degradation - Time and Temperature', '%'],
+  ['Original Battery Degradation - Cycling', '%'],
+  ['Original Battery Equivalent Cycles', 'cycles'],
+  ['AC Required Operating Capacity', 'kW'],
+  ['DC Required Operating Capacity', 'kW'],
+  ['AC Operating Capacity', 'kW'],
+  ['DC Operating Capacity', 'kW'],
+]
+export const applianceHeaderUnits = [
+  ['newAppliancesLoad', 'kW'],
+  ['productionUnitsRevenue', '$'],
+  ['day_hour', '-'],
+  ['kw_factor', '-'],
 ]
 
-export const homerHeaderUnits = {
-  // 'Time': '-',
-  'Global Solar': 'kW/m2',
-  'PV Solar Altitude': 'Â°',
-  'PV Solar Azimuth': 'Â°',
-  'PV Angle of Incidence': 'Â°',
-  'PV Incident Solar': 'kW/m2',
-  'PV Power Output': 'kW',
-  'AC Primary Load': 'kW',
-  'AC Primary Load Served': 'kW',
-  'Total Electrical Load Served': 'kW',
-  'Renewable Penetration': '%',
-  'Original Excess Electrical Production': 'kW',
-  // 'Unmet Electrical Load': 'kW',
-  'Capacity Shortage': 'kW',
-  'Total Renewable Power Output': 'kW',
-  totalElectricalProduction: 'kW',
-  originalElectricalProductionLoadDiff: 'kW',
-  originalBatteryEnergyContent: 'kWh',
-  originalModeledBatteryEnergyContent: 'kWh',
-  'Inverter Power Input': 'kW',
-  'Inverter Power Output': 'kW',
-  'Rectifier Power Input': 'kW',
-  'Rectifier Power Output': 'kW',
-  'Battery Maximum Charge Power': 'kW',
-  'Battery Maximum Discharge Power': 'kW',
-  'Battery Charge Power': 'kW',
-  'Battery Discharge Power': 'kW',
-  'Battery Input Power': 'kW',
-  'Battery State of Charge': '%',
-  'Battery Energy Cost': '$/kWh',
-  'Battery Temperature': 'C',
-  'Battery Degradation - Time and Temperature': '%',
-  'Battery Degradation - Cycling': '%',
-  'Battery Equivalent Cycles': 'cycles',
-  'AC Required Operating Capacity': 'kW',
-  'DC Required Operating Capacity': 'kW',
-  'AC Operating Capacity': 'kW',
-  'DC Operating Capacity': 'kW',
-  // 'hour',
+// Export array of columnn headers. This is to allow defining column order
+const combinedColumns = hybridColumnHeaderUnits.concat(homerHeaderUnits, applianceHeaderUnits)
+export const combinedColumnHeaderOrder = _.map(combinedColumns, columnPair => {
+  return _.first(columnPair)
+})
+
+// Export object that maps units of the columns to the column header name.
+// This is used in the datatable
+export const combinedColumnHeaderUnits = _.fromPairs(combinedColumns)
+
+// Export an object that maps the column header name to the table type. This can
+// be used to color the columns based on table type. This is done for very fast lookup
+const hybridColumnTypes = _(hybridColumnHeaderUnits)
+  .fromPairs()
+  .mapValues(val => 'hybridColumns')
+  .value()
+const homerColumnTypes = _(homerHeaderUnits)
+  .fromPairs()
+  .mapValues(val => 'homer')
+  .value()
+const applianceColumnTypes = _(applianceHeaderUnits)
+  .fromPairs()
+  .mapValues(val => 'appliance')
+  .value()
+
+export const columnHeaderByTableType = {
+  ...hybridColumnTypes,
+  ...homerColumnTypes,
+  ...applianceColumnTypes,
 }
-
-export const applianceHeaders = [
-  'newAppliancesLoad',
-  'productionUnitsRevenue',
-  // 'datetime',
-  // 'hour',
-  // 'day',
-  // 'hour_of_day',
-  'day_hour',
-  'kw_factor',
-  // 'production_factor',
-]
-
-export const applianceHeaderUnits = {
-  newAppliancesLoad: 'kW',
-  productionUnitsRevenue: '$',
-  // 'datetime',
-  // 'hour',
-  // 'day',
-  // 'hour_of_day',
-  day_hour: '-',
-  kw_factor: '-',
-  // 'production_factor',
-}
-
-export const combinedColumnHeaderOrder = hybridColumnHeaders.concat(homerHeaders, applianceHeaders)
-
-export const combinedColumnHeaderUnits = {
-  ...hybridColumnHeaderUnits,
-  ...homerHeaderUnits,
-  ...applianceHeaderUnits,
-}
-
-export function setColumnHeaderTableType(hybridColumnHeaders, homerHeaders, applianceHeaders) {
-  const hybridColumns = _.reduce(
-    hybridColumnHeaders,
-    (result, header) => {
-      result[header] = 'hybridColumns'
-      return result
-    },
-    {}
-  )
-  const homer = _.reduce(
-    homerHeaders,
-    (result, header) => {
-      result[header] = 'homer'
-      return result
-    },
-    {}
-  )
-  const appliance = _.reduce(
-    applianceHeaders,
-    (result, header) => {
-      result[header] = 'appliance'
-      return result
-    },
-    {}
-  )
-  return {
-    ...hybridColumns,
-    ...homer,
-    ...appliance,
-  }
-}
-
-export const columnHeaderByTableType = setColumnHeaderTableType(
-  hybridColumnHeaders,
-  homerHeaders,
-  applianceHeaders
-)
