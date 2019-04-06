7c21cd1174dd7eb14b34ed6166f1f77ba429af46
Jeff Friesen <momoblackblack@gmail.com>
2019-04-06 19:08:44 +0700

    Make battery energy content charts much nicer
    Also including Excess production and unmet load, since they are so closely related

--- src/components/Main/BatteryEnergyContent/index.js
+++ src/components/Main/BatteryEnergyContent/index.js
@@ -1,28 +1,18 @@
 import * as React from 'react'
 import { observer, inject } from 'mobx-react'
 import _ from 'lodash'
+import { Header } from 'semantic-ui-react'
 import LoaderSpinner from '../../../components/Elements/Loader'
 import {
-  // Line,
   XAxis,
   YAxis,
   Tooltip,
   Brush,
-  Legend,
   ResponsiveContainer,
   AreaChart,
   Area,
+  ReferenceLine,
 } from 'recharts'
-import { greyColors } from '../../../utils/constants'
-
-function gradientOffset(min, max) {
-  if (max <= 0) {
-    return 0
-  } else if (min >= 0) {
-    return 1
-  }
-  return max / (max - min)
-}
 
 class BatteryEnergyContent extends React.Component {
   render() {
@@ -31,37 +21,91 @@ class BatteryEnergyContent extends React.Component {
       return <LoaderSpinner />
     }
     const { batteryMinEnergyContent, batteryMaxEnergyContent } = activeGrid
-    const off = gradientOffset(batteryMinEnergyContent, batteryMaxEnergyContent)
-    console.log('off: ', off)
+    const brushStartDomain = [0, 200]
     return (
       <div>
-        <h3>
+        {/*<h3>
           Battery Energy Content by hour of year <small style={{ color: greyColors[1] }}>kWh</small>
-        </h3>
+        </h3>*/}
+
+        <ChartHeader title="Excess Production" />
+        <ResponsiveContainer minWidth={1000} minHeight={100} height="90%">
+          <AreaChart
+            width={600}
+            height={100}
+            data={hybridColumns}
+            syncId="anyId"
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <YAxis label={{ value: 'kWh', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="newExcessProduction"
+              stroke={'rgb(102, 102, 102)'}
+              strokeWidth={0.5}
+              fill="#82ca9d"
+            />
+            <Brush startIndex={brushStartDomain[0]} endIndex={brushStartDomain[1]} height={0} />
+          </AreaChart>
+        </ResponsiveContainer>
+
+        <ChartHeader title="Battery Energy Content" />
         <ResponsiveContainer minWidth={1000} minHeight={400} height="90%">
           <AreaChart
             width={600}
             height={400}
             data={hybridColumns}
+            syncId="anyId"
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <XAxis dataKey="hour" />
-            <YAxis />
+            <YAxis label={{ value: 'kWh', angle: -90, position: 'insideLeft' }} />
             <Tooltip />
-            <defs>
-              <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                <stop offset={off} stopColor="rgb(33, 186, 69, 0.8)" stopOpacity={1} />
-                <stop offset={off} stopColor="red" stopOpacity={1} />
-              </linearGradient>
-            </defs>
             <Area
               type="monotone"
               dataKey="batteryEnergyContent"
               stroke={'rgb(102, 102, 102)'}
               strokeWidth={0.5}
-              fill="url(#splitColor)"
+              fill="#8884d8"
             />
-            <Legend />
-            <Brush startIndex={0} endIndex={200} />
+            <ReferenceLine
+              y={batteryMinEnergyContent}
+              stroke="red"
+              label={`Min Battery Energy Content: ${_.round(batteryMinEnergyContent, 2)}`}
+              strokeDasharray="3 3"
+            />
+            <ReferenceLine
+              y={batteryMaxEnergyContent}
+              label={`Max Battery Energy Content: ${_.round(batteryMaxEnergyContent, 2)}`}
+              stroke="red"
+              strokeDasharray="3 3"
+            />
+            <Brush
+              startIndex={brushStartDomain[0]}
+              endIndex={brushStartDomain[1]}
+              height={0}
+              gap={5}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+
+        <ChartHeader title="Unmet Load" />
+        <ResponsiveContainer minWidth={1000} minHeight={100} height="90%">
+          <AreaChart
+            width={600}
+            height={100}
+            data={hybridColumns}
+            syncId="anyId"
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <YAxis label={{ value: 'kWh', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="newUnmetLoad"
+              stroke={'rgb(102, 102, 102)'}
+              strokeWidth={0.5}
+              fill="#ffc658"
+            />
+            <Brush startIndex={brushStartDomain[0]} endIndex={brushStartDomain[1]} gap={5} />
           </AreaChart>
         </ResponsiveContainer>
       </div>
@@ -70,3 +114,17 @@ class BatteryEnergyContent extends React.Component {
 }
 
 export default inject('store')(observer(BatteryEnergyContent))
+
+const chartLabelStyle = {
+  marginLeft: '60px',
+  marginTop: '10px',
+  marginBottom: 0,
+}
+
+const ChartHeader = ({ title }) => {
+  return (
+    <Header as="h3" style={chartLabelStyle}>
+      {title}
+    </Header>
+  )
+}
