e529bc57ecfee16e8a1d8968d42fe659ba6fc695
Jeff Friesen <momoblackblack@gmail.com>
2019-04-25 21:36:33 -0600

    Stub out chart for Time Segment view

---
+++ src/components/Main/TimeSegments/TimeSegmentControls.js
@@ -0,0 +1,94 @@
+import * as React from 'react'
+import _ from 'lodash'
+import { observer, inject } from 'mobx-react'
+import { Grid, Button, Header } from 'semantic-ui-react'
+import LoaderSpinner from '../../../components/Elements/Loader'
+
+class TimeSegmentControls extends React.Component {
+  state = { stackOffset: 'none' }
+
+  handleMetricChange = (value, e) => {
+    e.preventDefault()
+    return null
+  }
+
+  handleTimeChange = (value, e) => {
+    e.preventDefault()
+    return null
+  }
+
+  handleStackClick = (value, e) => {
+    e.preventDefault()
+    this.setState({ stackOffset: value })
+  }
+
+  render() {
+    const { combinedTable } = this.props.store
+    if (_.isEmpty(combinedTable)) {
+      return <LoaderSpinner />
+    }
+    const { stackOffset } = this.state
+    const { maxLoadValue, maxLoadFirstHour } = this.props.store.maxApplianceLoad
+    return (
+      <Grid>
+        <Grid.Row style={{ paddingBottom: 0 }}>
+          <Grid.Column width={2} textAlign="right" style={{ marginTop: '6px' }}>
+            <strong>Select Metric</strong>
+          </Grid.Column>
+          <Grid.Column width={14}>
+            <Button.Group basic compact>
+              <Button onClick={this.handleMetricChange.bind(null, 'avgLoad')}>Average Load</Button>
+              <Button onClick={this.handleMetricChange.bind(null, 'unmetLoadCount')}>
+                Unmet Load Count
+              </Button>
+              <Button onClick={this.handleMetricChange.bind(null, 'avgUnmetLoad')}>
+                Average Unmet Load
+              </Button>
+              <Button onClick={this.handleMetricChange.bind(null, 'avgExcessProduction')}>
+                Average Excess Production
+              </Button>
+            </Button.Group>
+            <Button.Group basic compact style={{ float: 'right' }}>
+              <Button
+                onClick={this.handleStackClick.bind(null, 'none')}
+                active={stackOffset === 'none'}>
+                Normal Stacked
+              </Button>
+              <Button
+                onClick={this.handleStackClick.bind(null, 'expand')}
+                active={stackOffset === 'expand'}>
+                100% Stacked
+              </Button>
+            </Button.Group>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={2} textAlign="right" style={{ marginTop: '6px' }}>
+            <strong>By</strong>
+          </Grid.Column>
+          <Grid.Column width={14}>
+            <Button.Group basic compact>
+              <Button onClick={this.handleTimeChange.bind(null, 'hourOfDay')}>Hour of Day</Button>
+              <Button onClick={this.handleTimeChange.bind(null, 'dayOfWeek')}>Day of Week</Button>
+              <Button onClick={this.handleTimeChange.bind(null, 'month')}>Month</Button>
+              <Button onClick={this.handleTimeChange.bind(null, 'dayHour')}>Day-Hour</Button>
+            </Button.Group>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={7}>
+            <Header as="h3">
+              Loads by hour of year
+              <Header sub>Each data point unit is average kW for 1 hour (kW*h)</Header>
+            </Header>
+          </Grid.Column>
+          <Grid.Column width={9} />
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
+
+export default inject('store')(observer(TimeSegmentControls))
--- src/components/Main/TimeSegments/index.js
+++ src/components/Main/TimeSegments/index.js
@@ -7,166 +7,38 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-  Brush,
+  Bar,
+  BarChart,
   Legend,
   ResponsiveContainer,
-  AreaChart,
-  Area,
-  ReferenceLine,
+  // AreaChart,
+  // Area,
+  // ReferenceLine,
 } from 'recharts'
 import { formatDateForTable } from '../../../utils/helpers'
-import { chartColorsByKey } from '../../../utils/constants'
+import { getChartColors } from '../../../utils/constants'
 import { fieldDefinitions } from '../../../utils/fieldDefinitions'
+import TimeSegmentControls from './TimeSegmentControls'
 
 class TimeSegments extends React.Component {
-  state = { stackOffset: 'none' }
-
-  handleMetricChange = (value, e) => {
-    e.preventDefault()
-    return null
-  }
-
-  handleTimeChange = (value, e) => {
-    e.preventDefault()
-    return null
-  }
-
-  handleStackClick = (value, e) => {
-    e.preventDefault()
-    this.setState({ stackOffset: value })
-  }
-
   render() {
-    const { combinedTable } = this.props.store
-    if (_.isEmpty(combinedTable)) {
+    const { summaryStats } = this.props.store
+    if (_.isEmpty(summaryStats)) {
       return <LoaderSpinner />
     }
-    const { stackOffset } = this.state
-    const { maxLoadValue, maxLoadFirstHour } = this.props.store.maxApplianceLoad
-    const yAxisDomainMax = _.ceil(maxLoadValue + maxLoadValue * 0.05, 2)
+    const { allUnmetLoadHist } = summaryStats
     return (
       <div>
-        <Grid>
-          <Grid.Row style={{ paddingBottom: 0 }}>
-            <Grid.Column width={2} textAlign="right" style={{ marginTop: '6px' }}>
-              <strong>Select Metric</strong>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <Button.Group basic compact>
-                <Button onClick={this.handleMetricChange.bind(null, 'avgLoad')}>
-                  Average Load
-                </Button>
-                <Button onClick={this.handleMetricChange.bind(null, 'unmetLoadCount')}>
-                  Unmet Load Count
-                </Button>
-                <Button onClick={this.handleMetricChange.bind(null, 'avgUnmetLoad')}>
-                  Average Unmet Load
-                </Button>
-                <Button onClick={this.handleMetricChange.bind(null, 'avgExcessProduction')}>
-                  Average Excess Production
-                </Button>
-              </Button.Group>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row>
-            <Grid.Column width={2} textAlign="right" style={{ marginTop: '6px' }}>
-              <strong>By</strong>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <Button.Group basic compact>
-                <Button onClick={this.handleTimeChange.bind(null, 'hourOfDay')}>Hour of Day</Button>
-                <Button onClick={this.handleTimeChange.bind(null, 'dayOfWeek')}>Day of Week</Button>
-                <Button onClick={this.handleTimeChange.bind(null, 'month')}>Month</Button>
-                <Button onClick={this.handleTimeChange.bind(null, 'dayHour')}>Day-Hour</Button>
-              </Button.Group>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row>
-            <Grid.Column width={7}>
-              <h3>
-                Loads by hour of year <br />
-                <small style={{ fontWeight: '300' }}>
-                  Each data point unit is average kW for 1 hour (kW*h)
-                </small>
-              </h3>
-            </Grid.Column>
-            <Grid.Column width={9}>
-              <Button.Group basic compact style={{ float: 'right', marginTop: '8px' }}>
-                <Button
-                  onClick={this.handleStackClick.bind(null, 'none')}
-                  active={stackOffset === 'none'}>
-                  Normal Stacked
-                </Button>
-                <Button
-                  onClick={this.handleStackClick.bind(null, 'expand')}
-                  active={stackOffset === 'expand'}>
-                  100% Stacked
-                </Button>
-              </Button.Group>
-              <Table basic="very" compact collapsing style={{ marginTop: 0 }}>
-                <Table.Body>
-                  <Table.Row>
-                    <Table.Cell>Max Appliance Load</Table.Cell>
-                    <Table.Cell textAlign="right">{_.round(maxLoadValue, 2)} kW*h</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>First hour of year hitting max load</Table.Cell>
-                    <Table.Cell textAlign="right">{maxLoadFirstHour}</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
-              </Table>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <TimeSegmentControls />
         <ResponsiveContainer minWidth={1000} minHeight={400} height="90%">
-          <AreaChart
-            data={combinedTable}
-            stackOffset={stackOffset}
-            margin={{ top: 40, right: 30, left: 0, bottom: 0 }}>
-            <XAxis dataKey="hour" />
-            <YAxis domain={stackOffset === 'none' ? [0, yAxisDomainMax] : null} />
+          <BarChart data={allUnmetLoadHist} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <XAxis dataKey="hour_of_day" />
+            <YAxis />
             <Tooltip content={<CustomToolTip />} />
-            <Area
-              type="monotone"
-              dataKey="Original Electrical Load Served"
-              stackId="1"
-              stroke={chartColorsByKey['Original Electrical Load Served']}
-              fill={chartColorsByKey['Original Electrical Load Served']}
-              fillOpacity="1"
-            />
-            <Area
-              type="monotone"
-              dataKey="newAppliancesLoad"
-              stackId="1"
-              stroke={chartColorsByKey['newAppliancesLoad']}
-              fill={chartColorsByKey['newAppliancesLoad']}
-              fillOpacity="1"
-            />
-            <Area
-              type="monotone"
-              dataKey="newAppliancesAncillaryLoad"
-              stackId="1"
-              stroke={chartColorsByKey['newAppliancesAncillaryLoad']}
-              fill={chartColorsByKey['newAppliancesAncillaryLoad']}
-              fillOpacity="1"
-            />
-            <ReferenceLine
-              x={maxLoadFirstHour}
-              stroke={chartColorsByKey['newAppliancesAncillaryLoad']}
-              label="First Hour Max Load"
-              strokeDasharray="3 3"
-            />
-            <ReferenceLine
-              y={maxLoadValue}
-              label={`Max Appliance Load: ${_.round(maxLoadValue, 2)}`}
-              stroke={chartColorsByKey['newAppliancesAncillaryLoad']}
-              strokeDasharray="3 3"
-            />
             <Legend />
-            <Brush startIndex={0} endIndex={200} />
-          </AreaChart>
+            <Bar dataKey="originalUnmetLoad" fill={getChartColors('originalUnmetLoad')} />
+            <Bar dataKey="totalUnmetLoad" fill={getChartColors('totalUnmetLoad')} />
+          </BarChart>
         </ResponsiveContainer>
       </div>
     )
