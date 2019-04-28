38be89b8fb9b98127dfdc8de1236df7701f15dd2
Jeff Friesen <momoblackblack@gmail.com>
2019-04-27 18:26:15 -0600

    Remove broken background image from loader

--- src/components/Elements/Loader/index.js
+++ src/components/Elements/Loader/index.js
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+import { Dimmer, Loader, Segment } from 'semantic-ui-react'
 
 const LoaderSpinner = () => (
   <Segment>
     <Dimmer active style={{ height: '200px' }}>
       <Loader>Loading</Loader>
     </Dimmer>
-    <Image src="https://react.semantic-ui.com/src/images/wireframe/short-paragraph.png" />
   </Segment>
 )
 
