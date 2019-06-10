83aa4ece275fe82cc31a642445400815c943c15b
Jeff Friesen <momoblackblack@gmail.com>
2019-06-10 09:39:27 -0600

    Changing labels of tabbed area

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -13,8 +13,8 @@ import AncillaryEquipment from './AncillaryEquipment'
 import DataTable from './DataTable'
 import Loads from './Loads'
 import BatteryEnergyContent from './BatteryEnergyContent'
-import TimeSegments from './TimeSegments'
-import GridComponents from './GridComponents'
+import GridAnalysis from './TimeSegments'
+import SystemComponents from './GridComponents'
 
 const Main = ({ match, store }) => {
   const { activeGrid } = store
@@ -26,25 +26,25 @@ const Main = ({ match, store }) => {
       <FileChooser />
 
       <Menu secondary pointing>
-        <Menu.Item as={NavItem} to={`/`} name="Summary" />
+        <Menu.Item as={NavItem} to={`/`} name="Results" />
         <Menu.Item as={NavItem} to={`/tool/components`} name="System Components" />
         <Menu.Item as={NavItem} to={`/tool/ancillary`} name="Ancillary Equipment" />
-        <Menu.Item as={NavItem} to={`/tool/datatable`} name="Data" />
-        <Menu.Item as={NavItem} to={`/tool/loads`} name="Loads" />
-        <Menu.Item as={NavItem} to={`/tool/time-segments`} name="Grid Analysis" />
+        <Menu.Item as={NavItem} to={`/tool/grid-analysis`} name="Grid Analysis" />
         <Menu.Item as={NavItem} to={`/tool/battery-energy-content`} name="Battery Analysis" />
+        <Menu.Item as={NavItem} to={`/tool/loads`} name="Load Analysis" />
+        <Menu.Item as={NavItem} to={`/tool/datatable`} name="Raw Data" />
       </Menu>
 
       <div className="mainContent">
         <Route exact path="/" component={Summary} />
         <Route exact path="/tool" component={Summary} />
         <Route path={`/tool/summary`} component={Summary} />
+        <Route path={`/tool/components`} component={SystemComponents} />
         <Route path={`/tool/ancillary`} component={AncillaryEquipment} />
-        <Route path={`/tool/datatable`} component={DataTable} />
-        <Route path={`/tool/loads`} component={Loads} />
-        <Route path={`/tool/time-segments`} component={TimeSegments} />
+        <Route path={`/tool/grid-analysis`} component={GridAnalysis} />
         <Route path={`/tool/battery-energy-content`} component={BatteryEnergyContent} />
-        <Route path={`/tool/components`} component={GridComponents} />
+        <Route path={`/tool/loads`} component={Loads} />
+        <Route path={`/tool/datatable`} component={DataTable} />
       </div>
     </div>
   )
